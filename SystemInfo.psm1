fde233dc35e65024f10534ee73c5384e00b0b918
SAGSA <sergey_abakan@bk.ru>
2017-12-28 16:49:05 +0700

    Updated

--- SystemInfo.psm1
+++ SystemInfo.psm1
@@ -134,7 +134,7 @@ function Get-SystemInfo
             [switch]$AppendToResult,
             [switch]$ShowStatistics,
             [ValidateSet("*","OsVersion","OSArchitecture","OsCaption","OsInstallDate","OsUpTime","OsLoggedInUser","OsProductKey","MemoryTotal","MemoryFree","MemoryModules","MemoryModInsCount",
-            "MemoryMaxIns","MemorySlots","ECCType","MemoryAvailable","Motherboard","MotherboardModel","DeviceModel","Cdrom","CdromMediatype","HddDevices","HddDevCount",
+            "MemoryMaxIns","MemorySlots","ECCType","MemoryAvailable","Motherboard","MotherboardModel","DeviceModel","Cdrom","CdromMediatype","HddDevices","HddDevCount","HDDSmart",
             "HddPredictFailure","VideoModel","VideoRam","VideoProcessor","CPUName","CPUSocket","MaxClockSpeed","CPUCores","CPULogicalCore","MonitorManuf",
             "MonitorPCode","MonitorSN","MonitorName","MonitorYear","NetPhysAdapCount","NetworkAdapters","Printers","IsPrintServer","UsbConPrOnline","UsbDevices","CPULoad","SoftwareList","RegistryValue","OsAdministrators","OsActivationStatus")] 
             [array]$Properties
@@ -167,7 +167,7 @@ MotherboardModel=   '-Class Win32_BaseBoard -Property Product'
 DeviceModel=        '-Class Win32_Computersystem -Property model'
 Cdrom=              '-Class Win32_CDROmDrive -Property Caption'
 CdromMediatype=     '-Class Win32_CDROMDrive -Property MediaType'
-HddDevices=         '-Class Win32_DiskDrive,MSStorageDriver_FailurePredictStatus -ScriptBlock $SbHddDevices'
+HddDevices=         '-Class Win32_DiskDrive,MSStorageDriver_FailurePredictStatus,MSStorageDriver_FailurePredictData -ScriptBlock $SbHddDevices'
 HddDevCount=        '-Class Win32_DiskDrive -ScriptBlock $SbHddDevCount'
 HddPredictFailure=  '-Class MSStorageDriver_FailurePredictStatus -Property PredictFailure'
 VideoModel=         '-Class Win32_VideoController     -ScriptBlock $SbVideoModel'
@@ -197,11 +197,13 @@ OsProductKey=       '-Class StdRegProv -ScriptBlock $SBOsProductKey -UseRunspace
 OsLoggedInUser=     '-Class Win32_ComputerSystem -Property UserName'
 OsAdministrators=   '-Class Win32_OperatingSystem -Scriptblock $SbOsAdministrators -UseRunspace'
 OsActivationStatus= '-Query Select * From SoftwareLicensingProduct Where ApplicationID = "55c92734-d682-4d71-983e-d6ec3f16059f" And Licensestatus > 0 -ScriptBlock $SbOsActivationStatus'
+HDDSmart=           '-Class MSStorageDriver_FailurePredictStatus,MSStorageDriver_FailurePredictData,Win32_DiskDrive -ScriptBlock $SbHddSmart'
 }
 
 $ManualNamespace=@{
 wmiMonitorID='-Namespace Root\wmi'
 MSStorageDriver_FailurePredictStatus='-Namespace Root\wmi'
+MSStorageDriver_FailurePredictData='-Namespace Root\wmi'
 StdRegProv='-Namespace ROOT\default -Query SELECT * FROM meta_class WHERE __class="StdRegProv"'
 }
 
@@ -231,6 +233,177 @@ $ExcludeParam="Verbose","AppendToResult","Debug","ShowStatistics"
 
 #ScriptBlock
 #################################################################################################################################
+[scriptblock]$SbHddSmart=
+{
+	function ConvertTo-Hex ( $DEC ) {
+		'{0:x2}' -f [int]$DEC
+	}
+	function ConvertTo-Dec ( $HEX ) {
+		[Convert]::ToInt32( $HEX, 16 )
+	}
+	function Get-AttributeDescription ( $Value ) {
+		switch ($Value) {
+			'01' { 'Raw Read Error Rate' }
+			'02' { 'Throughput Performance' }
+			'03' { 'Spin-Up Time' }
+			'04' { 'Number of Spin-Up Times (Start/Stop Count)' }
+			'05' { 'Reallocated Sector Count' }
+			'07' { 'Seek Error Rate' }
+			'08' { 'Seek Time Performance' }
+			'09' { 'Power On Hours Count (Power-on Time)' }
+			'0a' { 'Spin Retry Count' }
+			'0b' { 'Calibration Retry Count (Recalibration Retries)' }
+			'0c' { 'Power Cycle Count' }
+			'aa' { 'Available Reserved Space' }
+			'ab' { 'Program Fail Count' }
+			'ac' { 'Erase Fail Count' }
+			'ae' { 'Unexpected power loss count' }
+			'b7' { 'SATA Downshift Error Count' }
+			'b8' { 'End-to-End Error' }
+			'bb' { 'Reported Uncorrected Sector Count (UNC Error)' }
+			'bc' { 'Command Timeout' }
+			'bd' { 'High Fly Writes' }
+			'be' { 'Airflow Temperature' }
+			'bf' { 'G-Sensor Shock Count (Mechanical Shock)' }
+			'c0' { 'Power Off Retract Count (Emergency Retry Count)' }
+			'c1' { 'Load/Unload Cycle Count' }
+			'c2' { 'Temperature' }
+			'c3' { 'Hardware ECC Recovered' }
+			'c4' { 'Reallocated Event Count' }
+			'c5' { 'Current Pending Sector Count' }
+			'c6' { 'Offline Uncorrectable Sector Count (Uncorrectable Sector Count)' }
+			'c7' { 'UltraDMA CRC Error Count' }
+			'c8' { 'Write Error Rate (MultiZone Error Rate)' }
+			'c9' { 'Soft Read Error Rate' }
+			'cb' { 'Run Out Cancel' }
+			'cа' { 'Data Address Mark Error' }
+			'dc' { 'Disk Shift' }
+			'e1' { 'Load/Unload Cycle Count' }
+			'e2' { 'Load ''In''-time' }
+			'e3' { 'Torque Amplification Count' }
+			'e4' { 'Power-Off Retract Cycle' }
+			'e8' { 'Available Reserved Space2' }
+			'e9' { 'Media Wearout Indicator' }
+			'f0' { 'Head Flying Hours' }
+			'f1' { 'Total LBAs Written' }
+			'f2' { 'Total LBAs Read' }
+			'f9' { 'NAND Writes (1GiB)' }
+			'fe' { 'Free Fall Protection' }
+			default { $Value }
+		}
+	}
+
+$PnpDev=@{}
+$hdddev=$Win32_DiskDrive | Select-Object Model,InterfaceType,FirmwareRevision,PNPDeviceID
+$hdddev | foreach {
+    $PnpDev.Add($($_.pnpdeviceid -replace "\\","\\"),$_)
+}
+
+$PnpDev.Keys | foreach {
+    $PnpDevid=$_
+    $TmpFailData=$MSStorageDriver_FailurePredictData | Where-Object  {$_.InstanceName -Match $PnpDevid}
+    $TmpFailStat=$MSStorageDriver_FailurePredictStatus | Where-Object  {$_.InstanceName -Match $PnpDevid}
+    if ($TmpFailStat)
+    {
+        $PnpDev[$PnpDevid] | Add-Member -MemberType NoteProperty -Name  PredictFailure -Value $TmpFailStat.PredictFailure
+    }
+    else
+    {
+        $PnpDev[$PnpDevid] | Add-Member -MemberType NoteProperty -Name  PredictFailure -Value 'Not supported'
+    }
+    if ($TmpFailData)
+    {
+        $Disk=$TmpFailData
+        $i = 0
+        $Report = @()
+        $pByte = $null
+		        foreach ( $Byte in $Disk.VendorSpecific ) {
+			        $i++
+			        if (( $i - 3 ) % 12 -eq 0 ) 
+                    {
+				        if ( $Byte -eq 0) { break }
+				        $Attribute = '{0:x2}' -f [int]$Byte
+			        } 
+                    else 
+                    {
+				        $post = ConvertTo-Hex $pByte
+				        $pref = ConvertTo-Hex $Byte
+				        $Value = ConvertTo-Dec "$pref$post"
+				        if (( $i - 3 ) % 12 -eq 6 ) 
+                        {
+					        if ( $Attribute -eq '09' ) { [int]$Value = $Value / 24 }
+					        #$Report += [PSCustomObject]@{ Name = $( Get-AttributeDescription $Attribute ); Value = $Value }
+				            $PnpDev[$PnpDevid] | Add-Member -MemberType NoteProperty -Name $( Get-AttributeDescription $Attribute) -Value $Value
+                        }
+			        }
+			        $pByte = $Byte
+                }
+
+        #$Report
+        
+    }
+    else
+    {
+        $PnpDev[$PnpDevid] | Add-Member -MemberType NoteProperty -Name SmartStatus -Value 'Not supported' 
+    }
+    $HddSmart=$PnpDev[$PnpDevid]
+    $WarningThreshold=@{
+    "Temperature"=45,54
+    "Reallocated Sector Count"=1,10
+    }
+    $CriticalThreshold=@{
+    "Temperature"=55
+    "Reallocated Sector Count"=11
+    }
+        $HddWarning=$False
+        $HddCritical=$False
+        $HddSmart | Get-Member | foreach {
+            $Property=$_.name
+            if ($WarningThreshold[$Property])
+            {
+                $MinWarningThreshold=$WarningThreshold[$Property][0]
+                $MaxWarningThreshold=$WarningThreshold[$Property][1]
+                    if ($HddSmart.$Property -le $MaxWarningThreshold -and $HddSmart.$Property -ge $MinWarningThreshold)
+                    {
+                        $HddWarning=$true
+                    }
+            }
+            
+
+            if ($CriticalThreshold[$Property])
+            {
+                $MinCriticalThreshold=$CriticalThreshold[$Property]
+                    if($HddSmart.$Property -ge $MinCriticalThreshold)
+                    {
+                        $HddCritical=$true
+                    } 
+            }
+              
+            
+        #End Foreach
+        }
+    if ($HddSmart.smartstatus -ne "Not supported")
+    {
+        if ($HddWarning)
+        {
+            $HddSmart | Add-Member -MemberType NoteProperty -Name SmartStatus -Value 'Warning' 
+        }
+        elseif($HddCritical -or $HddSmart.PredictFailure)
+        {
+            $HddSmart | Add-Member -MemberType NoteProperty -Name SmartStatus -Value 'Critical'   
+        }
+        else
+        {
+            $HddSmart | Add-Member -MemberType NoteProperty -Name SmartStatus -Value 'Ok'   
+        }
+    }
+$HddSmart
+#End Foreach
+}
+
+
+}
+
 [scriptblock]$SbOsActivationStatus={
 
 $ActStat=@{
@@ -1548,6 +1721,16 @@ $AllProperties | foreach{
             <ListItem>
                 <PropertyName>$Property</PropertyName>
             </ListItem>"
+    }
+    elseif($ForObject.HddSmart)
+    {
+        $XmlFormatList+="
+        <ListItem>
+        <Label>$Property</Label>
+            <ScriptBlock> 
+                $DollarUnder.$Property | fl | out-string
+            </ScriptBlock>
+        </ListItem>"
     }
     elseif ($FormatTableFor -eq ($ForObjectProperty).GetType().name)
     {
